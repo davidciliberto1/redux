48dba318c6c71bb1da168381d4a04ee800cacdf7
davidciliberto1 <davidciliberto@gmail.com>
2020-08-12 20:49:42 -0300

    practica redux

---
+++ src/components/details/page.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+
+
+function Page(props) {
+    return (
+        <div>
+            <Fragment>
+                Details
+            </Fragment>
+        </div>
+    )
+}
+
+export default Page;
---
+++ src/components/results/page.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+
+
+function Page(props) {
+    return (
+        <div>
+            <Fragment>
+                Results
+            </Fragment>
+        </div>
+    )
+}
+
+export default Page;
